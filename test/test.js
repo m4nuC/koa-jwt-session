03395fb0c2ba75a0ccd7745c97149ced1250462e
Emmanuel <m4nu@hotmail.fr>
2015-09-12 15:55:00 +0800

    Add test boilerplate

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var koa = require('koa');
+var request = require('supertest');
+var should = require('should');
+var session = require('..');
+
+describe('Koa JWT Session', function(){
+	it('should work', function(done){
+		//
+	})
+})
