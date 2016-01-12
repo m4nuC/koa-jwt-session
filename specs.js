518e3389ba718cb4e15ac545c50f46e9ea76f243
m4nuC <m4nu@hotmail.fr>
2016-01-12 11:08:31 +0800

    Add prepublish compilation

--- specs.js
+++ specs.js
@@ -1,6 +1,8 @@
+require("babel-core/register");
+require("babel-polyfill");
 const koa = require('koa');
 const expect = require('expect')
-const session = require('./');
+const session = require('./src');
 
 describe('Koa JWT Session', function(){
   it('should work', function(){
